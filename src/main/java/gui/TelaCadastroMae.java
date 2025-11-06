dfc1e2d1ec67b3d451bdf5cc8874fd1aae259d37
Gustavo Bueno da Silva <gustavo.sjc.silva@gmail.com>
2025-11-06 00:05:26 -0300

    feat(ui): Implementa tema FlatLaf, ícone customizado e corrige DB

--- src/main/java/gui/TelaCadastroMae.java
+++ src/main/java/gui/TelaCadastroMae.java
@@ -139,6 +139,13 @@ public class TelaCadastroMae extends JFrame {
     }
 
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(() -> new TelaCadastroMae().setVisible(true));
     }
 }
--- src/main/java/gui/TelaListaEncontros.java
+++ src/main/java/gui/TelaListaEncontros.java
@@ -119,6 +119,13 @@ public class TelaListaEncontros extends JFrame {
     }
 
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(() -> new TelaListaEncontros().setVisible(true));
     }
 }
